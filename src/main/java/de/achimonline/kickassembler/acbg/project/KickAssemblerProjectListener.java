dc6147aeddf7de43316961d440f9b1b3810d8d12
Achim <git@achimonline.de>
2020-07-26 19:47:37 +0200

    replace deprecated 'AbstractProjectComponent'

--- src/main/java/de/achimonline/kickassembler/acbg/project/KickAssemblerProjectListener.java
+++ src/main/java/de/achimonline/kickassembler/acbg/project/KickAssemblerProjectListener.java
@@ -1,10 +1,16 @@
 package de.achimonline.kickassembler.acbg.project;
 
-import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ex.ToolWindowManagerListener;
 
-public class KickAssemblerProjectListener extends AbstractProjectComponent {
-    protected KickAssemblerProjectListener(Project project) {
-        super(project);
+public class KickAssemblerProjectListener implements ToolWindowManagerListener {
+
+    private final Project project;
+
+    public KickAssemblerProjectListener(Project project) {
+        this.project = project;
     }
+
+    // TODO handle state-change ...
+
 }
