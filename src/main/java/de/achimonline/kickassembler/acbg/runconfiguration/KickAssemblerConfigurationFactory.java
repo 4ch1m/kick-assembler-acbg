78b0a22f9f774d49a082342c70eaec50e2090225
Achim <git@achimonline.de>
2020-07-27 16:07:26 +0200

    removal of deprecated 'ConfigurationFactoryEx'; removed unused 'onNewConfigurationCreated'-method

--- src/main/java/de/achimonline/kickassembler/acbg/runconfiguration/KickAssemblerConfigurationFactory.java
+++ src/main/java/de/achimonline/kickassembler/acbg/runconfiguration/KickAssemblerConfigurationFactory.java
@@ -1,7 +1,6 @@
 package de.achimonline.kickassembler.acbg.runconfiguration;
 
-import com.intellij.execution.RunManagerEx;
-import com.intellij.execution.configuration.ConfigurationFactoryEx;
+import com.intellij.execution.configurations.ConfigurationFactory;
 import com.intellij.execution.configurations.ConfigurationType;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.execution.configurations.RunConfigurationModule;
@@ -9,16 +8,9 @@ import com.intellij.openapi.project.Project;
 import de.achimonline.kickassembler.acbg.properties.KickAssemblerProperties;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
-
-public class KickAssemblerConfigurationFactory extends ConfigurationFactoryEx {
+public class KickAssemblerConfigurationFactory extends ConfigurationFactory {
     protected KickAssemblerConfigurationFactory(@NotNull ConfigurationType configurationType) {
         super(configurationType);
-    }
-
-    @Override
-    public void onNewConfigurationCreated(@NotNull RunConfiguration configuration) {
-        RunManagerEx.getInstanceEx(configuration.getProject()).setBeforeRunTasks(configuration, Collections.emptyList(), false);
     }
 
     @NotNull
