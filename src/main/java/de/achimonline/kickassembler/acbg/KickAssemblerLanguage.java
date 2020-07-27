d2ec9e435f06e1a5262cd53792b067f568ef4eda
Achim <git@achimonline.de>
2020-07-27 17:52:35 +0200

    unused import removed

--- src/main/java/de/achimonline/kickassembler/acbg/KickAssemblerLanguage.java
+++ src/main/java/de/achimonline/kickassembler/acbg/KickAssemblerLanguage.java
@@ -1,7 +1,6 @@
 package de.achimonline.kickassembler.acbg;
 
 import com.intellij.lang.Language;
-import de.achimonline.kickassembler.acbg.properties.KickAssemblerProperties;
 
 public class KickAssemblerLanguage extends Language {
     public static final KickAssemblerLanguage INSTANCE = new KickAssemblerLanguage();
